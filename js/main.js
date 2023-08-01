9a55f73c6b655f4923af12b3fe1e1bcade8a7e72
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-31 18:25:15 -0700

    findWinningCombination

--- js/main.js
+++ js/main.js
@@ -43,3 +43,23 @@ function updateTurn() {
     playerO.classList.add('active');
   }
 }
+// find winning combination
+function findWinningCombs() {
+  const winningCombs = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+  ];
+  winningCombs.forEach((winningComb) => {
+    const [a, b, c] = winningComb;
+    if (board[a] && board[a] == board[b] && board[a] == board[c]) {
+      return winningComb;
+    }
+  });
+  return null;
+}
